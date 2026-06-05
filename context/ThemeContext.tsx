4f62edf38bb9c1db89365d3269302d8f2e98670e
AndrysDM <andrysmoreno4@gmail.com>
2026-06-05 11:19:05 -0400

    .

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, ReactNode, useContext, useState } from 'react';
 
 type ThemeType = 'light' | 'dark';
 
